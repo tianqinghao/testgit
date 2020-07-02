af60b7e833065cb07950a9ea70fda44dd4a27e37
tianqinghao <tianqinghao4216@163.com>
2020-07-02 18:26:05 +0800

    第二个测试方法

--- src/test/java/testgit02.java
+++ src/test/java/testgit02.java
@@ -2,7 +2,7 @@ public class testgit02 {
     @Override
     public boolean equals(Object obj) {
         System.out.println("我来测试一下能不能用pull");
-
+        System.out.println("我来测试一下能不能用pull");
         return super.equals(obj);
     }
 }
