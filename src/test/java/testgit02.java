05e2525ca7eaf219f1825d74d89bc785c41f3275
tianqinghao <tianqinghao4216@163.com>
2020-07-02 18:45:48 +0800

    小圆测试

--- src/test/java/testgit02.java
+++ src/test/java/testgit02.java
@@ -3,6 +3,7 @@ public class testgit02 {
     public boolean equals(Object obj) {
         System.out.println("我来测试一下能不能用pull");
         System.out.println("我来测试一下能不能用pull");
+        System.out.println("小圆在分支我来测试一下能不能用pull");
         return super.equals(obj);
     }
 }
